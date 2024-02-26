c87774992980c572396014416e39563ada0fd4db
Arbaz Choudhary <ARBAZKHAN61@GMAIL.COM>
2024-02-27 00:26:18 +0530

    mern_realstate_project

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import { useState } from "react";
+
+/** @format */
+
+
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+        <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">
+          Better Data
+        </span>{" "}
+        Scalable AI.
+      </h1>
+      <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">
+        Here at Flowbite we focus on markets where technology, innovation, and
+        capital can unlock long-term value and drive economic growth.
+      </p>
+    </>
+  );
+}
+
+export default App;
